373707a4e42e4429c7b3b9551bc8fce15d58a0ff
darrylbalderas <drrylbalderas@gmail.com>
2020-12-17 11:03:28 -0800

    Add mock data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dame-agua
 
+
+docker-compose up --build --force-recreate --remove-orphans
+
+docker-compose down --remove-orphans
+
+
 ### Resources
+* [Mock data generator](https://www.mockaroo.com/)
+* [Information on docker-compose up ](https://docs.docker.com/compose/reference/up/)
--- sensor/app.py
+++ sensor/app.py
@@ -5,6 +5,7 @@ from itertools import cycle
 from pathlib import Path
 import json
 import time
+from pprint import pprint
 
 project_path = Path(__file__).parent
 
@@ -15,7 +16,7 @@ payloads = cycle(mock_data)
 
 database = 'plants'
 
-client = InfluxDBClient('localhost', 8086, database=database)
+client = InfluxDBClient(host='localhost', port=8086, database=database)
 client.create_database(database)
 
 for p in payloads:
@@ -29,8 +30,11 @@ for p in payloads:
             "fields": p
         }
     ]
-    time.sleep(1)
+    time.sleep(2)
     client.write_points(json_body)
-    result = client.query('select value from tropical;')
-    print(f"Result: {result}")
+    result = client.query('select * from tropical order by desc limit 1;')
 
+    print(client.get_list_database())
+    print(client.get_list_continuous_queries())
+    print(client.get_list_retention_policies())
+    print(client.get_list_measurements())
